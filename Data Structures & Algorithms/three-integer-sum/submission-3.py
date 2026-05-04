67965b682bc407a11417a7832cf99346d13372fd
Soul Byte <soul3147@proton.me>
2026-05-04 22:28:29 +0530

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        set(nums)
+        nums.sort()
+        left=0
+        right=len(nums)-1
+        ans=[]
+        odd_step=1
+        while right-left>=3:
+            sum_of_first_last=nums[left]+nums[right]
+            for k in range(left+1,right):
+                if k==(-1*sum_of_first_last):
+                    ans.append([nums[left],nums[k],nums[right]])
+            if odd_step:
+                left+=1
+                odd_step=0
+            else:
+                right-=1
+                odd_step=1
+        return ans

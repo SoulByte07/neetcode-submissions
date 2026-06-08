8ccf24a02674743ea5406bd9ea5a0a4c7f7159cf
Soul Byte <soul3147@proton.me>
2026-06-08 21:26:23 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        dup=[]
+        glb_max_len=0
+        count=0
+        for e in s:
+            if e not in dup:
+                count+=1
+                dup.append(e)
+            else:
+                glb_max_len=max(count,glb_max_len)
+                count=0
+                dup.clear()
+        return glb_max_len
+

4889a2178d1ab5ffaaadace4494a1531ec9fc57c
Soul Byte <soul3147@proton.me>
2026-07-28 21:21:39 +0530

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l=0
+        area=0
+        for r in range(1,len(height)):
+            if height[l]==0:
+                l+=1
+                continue
+            if height[l]<=height[r]:
+                length=min(height[l],height[r])
+                width=r-l-1
+                area+=(length*width)-(sum(height[l+1:r]))
+                l=r
+        return area

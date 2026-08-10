0828b2c2bd520c10eac45d53843f31feba0eb19b
Soul Byte <soul3147@proton.me>
2026-08-10 21:29:48 +0530

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.py
@@ -0,0 +1,25 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        eleStack=[]
+        indexStack=[]
+        res=[0]*len(temperatures) # init with 0's
+        for curr_i,e in enumerate(temperatures):
+            if len(eleStack)==0: # isEmpty()
+                eleStack.append(e)
+                indexStack.append(curr_i)
+                continue
+            topEle=eleStack[-1]
+            while topEle<e:
+                popedIndex=indexStack[-1]
+                gap=curr_i-popedIndex
+                res[popedIndex]=gap
+                eleStack.pop(-1)
+                indexStack.pop(-1)
+                if len(eleStack)==0: # isEmpty()
+                    break
+                else:                # Not Empty
+                    top=eleStack[-1]
+            eleStack.append(e)
+            indexStack.append(curr_i)
+        return res
+            

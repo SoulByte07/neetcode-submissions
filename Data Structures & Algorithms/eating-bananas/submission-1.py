674e24b848fe813e52b139371828f9357ebc0988
Soul Byte <soul3147@proton.me>
2026-08-17 19:40:50 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def computeEating(self, piles: List[int], k: int) -> int:
+        sumPile=0
+        for i,p in enumerate(piles):
+            # currPile=round(p/k)
+            currPile=(p + k - 1) // k
+            sumPile+=currPile
+        return sumPile
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        low,high=1,sum(piles) # lower and upper bound
+        minSumPile=high 
+        res=0 # min k
+        while low<=high:
+            mid=low+((high-low)//2) 
+            sumPile=self.computeEating(piles,mid)
+            if sumPile>h: # invalid and move left
+                low=mid+1
+            else: # valid and move right
+                if minSumPile>sumPile: # check the min
+                    res=mid # get the mid or k
+                minSumPile=min(minSumPile,sumPile) # get min
+                high=mid-1 # move right
+        return res
+
+            

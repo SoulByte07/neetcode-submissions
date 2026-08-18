75f184dc34461cd753d1e8e1aff457efe45921eb
Soul Byte <soul3147@proton.me>
2026-08-18 19:45:32 +0530

    Add: time-based-key-value-store - submission-1

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-1.py
@@ -0,0 +1,21 @@
+class TimeMap:
+
+    def __init__(self):
+        self.map=defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.map[key]=[[value,timestamp]]
+        
+
+    def get(self, key: str, timestamp: int) -> str:
+        res=""
+        val=self.map.get(key,[])
+        l,r=0,len(val)-1
+        while l<=r:
+            mid=(r+l)//2
+            if val[mid][1]<=timestamp:
+                res=val[mid][0]
+                l=mid+1
+            else:
+                r=mid-1
+        return res

d257951b214de43ed8780f53b95da55623761b2b
Soul Byte <soul3147@proton.me>
2026-08-23 20:15:39 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findDuplicate(self, n: List[int]) -> int:
+        si,fi=0,0
+        sv,fv=n[si],n[fi]
+        while True:
+            if sv==fv:
+                return n[si]
+            si=n[sv]
+            sv=n[si]
+            fi=n[sv]
+            fv=n[fi]

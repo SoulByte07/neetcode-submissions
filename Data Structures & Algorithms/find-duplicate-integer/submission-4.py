6470923f406d03e43329ce6ca160028c2fec2936
Soul Byte <soul3147@proton.me>
2026-08-23 20:24:10 +0530

    Add: find-duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow,fast=0,0
+        slow=nums[slow]
+        fast=nums[nums[fast]]
+        while slow!=fast:
+            slow=nums[slow]
+            fast=nums[nums[fast]]
+        return nums[nums[slow]]
